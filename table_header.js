4725c8e065e2004f447efbad7f78df9a26ca3a3e
haozhang <51780024+haozhangSWJTU@users.noreply.github.com>
2025-11-28 10:38:08 +0800

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -1,27 +1,22 @@
 document.write("<table width=\"850\" border=\"0\" height=\"184\">");
 document.write("<tbody>");
 document.write("<tr>");
-document.write("<td> <img src=\"Images/athens-lbz.JPG\" alt=\"Ben-Zheng Li\"");
+document.write("<td> <img src=\"Images/zhanghao.PNG\" alt=\"Hao Zhang"");
 document.write("width=\"350\" height=\"250\"> <\/td>");
 document.write("<td>");
-document.write("<h1>Ben-Zheng Li (李本正)</h1>");
+document.write("<h1>Hao Zhang 张浩</h1>");
 document.write("<br>");
-document.write("<b>Ph.D. Candidate</b>");
 document.write("<br>");
-
-document.write("<a href=\"https:\/\/www.uestc.edu.cn\"> University of Electronic Science and Technology of China <\/a></b>");
-              
-               
+      
 document.write("<br>");
 document.write("<br>");
 document.write("<b>Contact info:</b><br>");
 document.write(" Chengdu, Sichuan, China. <br>");
-document.write("E-mail: lbz1604179601@126.com, e1495946@u.nus.edu <br>");
+document.write("E-mail: aaronzhangfy@163.com, Wechat: 156811991O1<br>");
 document.write("<br>");
 document.write("&nbsp;");
-document.write("<a href=\"https://scholar.google.com/citations?hl=en&user=pXU2ncUAAAAJ&view_op=list_works&gmla=AJsN-F67h7paSgOvSMBZ6ntHaz0EriF3wdjLA39Iavpb_ywY90kAcAojuehiY1dxwVRZfS6E2RjZlSOrJXRwNW4iylxFynymZWzYLo9L7jDVGwGgJKWkakN5i1wqHE7YkUjofT6B_6uCkDPQOAVlLklQMGPL8Pvqkg\"><img src=\"Images\/scholar.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
-document.write("<a href=\"https:\/\/www.researchgate.net\/profile\/Ben\-Zheng\-Li\-2\"><img src=\"Images\/Researchgate.jpg\" alt=\"Researchgate\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
-document.write("<a href=\"https:\/\/github.com\/benzhengli\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https://www.researchgate.net/profile/Hao-Zhang-589\"><img src=\"Images\/Researchgate.jpg\" alt=\"Researchgate\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/github.com\/haozhangSWJTU\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
